2cdf6a4c22485c1ea62f01d92a208fc26c47dbdd
maththugalves <mathcontrabaixo@gmail.com>
2020-10-09 13:53:36 -0300

    undo the cake... never committed... stupid..

--- src copy/utils/getThemeColor.js
+++
@@ -1,8 +0,0 @@
-const getThemeColor = () => {
-    const theme = typeof window !== 'undefined' && window.__theme
-
-    if(theme === 'light') return '#f0f0f3'
-    if(theme === 'dark') return '#16202c'
-}
-
-export default getThemeColor
